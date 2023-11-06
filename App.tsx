12340f2aaaf56287f2bdecef2b711f184b84defd
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2023-11-06 16:09:15 -0300

    feat: add loading component and config fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
-import { Signin } from "./src/screens/Signin";
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import theme from "./src/theme";
 
+import { Signin } from "./src/screens/Signin";
+import Loading from "./src/components/Loading";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <Signin />;
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Signin />
     </ThemeProvider>
   );
 }
